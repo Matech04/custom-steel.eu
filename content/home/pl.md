dccee13aea4d7316876ccd9a17bacb331cf88820
Mateusz Chodacki <mateusz.chodacki@gmail.com>
2025-07-15 23:29:57 +0200

    fixes

---
+++ content/home/pl.md
@@ -0,0 +1,39 @@
+---
+title1: 'Twoje marzenia,'
+title2: nasze realizacje
+description1: >-
+  W Custom Steel łączymy rzemiosło z nowoczesną technologią, tworząc solidne
+  konstrukcje dopasowane do indywidualnych potrzeb. Od projektu po montaż – 
+description2: dbamy o każdy etap realizacji.
+button: Napisz do nas!
+cards:
+  card1:
+    title: Precyzja i jakość wykonania
+    description: >-
+      Korzystamy z nowoczesnych technologii i sprawdzonych metod, aby dostarczać
+      produkty trwałe, solidne i estetyczne
+  card2:
+    title: Indywidualne podejście do projektu
+    description: >-
+      Nie oferujemy gotowych rozwiązań. Dopasowujemy się do potrzeb, przestrzeni
+      i budżetu, realizując projekty skrojone na miarę.
+  card3:
+    title: Zaufany partner w realizacji
+    description: >-
+      Od pierwszego kontaktu po finalny montaż – możesz liczyć na nasze
+      wsparcie, terminowość i pełne zaangażowanie. 
+about:
+  title: Czym się zajmujemy?
+  description: >-
+    Custom Steel to dynamicznie rozwijająca się firma specjalizująca się w
+    realizacji indywidualnych zleceń na metalowe konstrukcje, takie jak drzwi,
+    ścianki, balustrady, meble, barierki oraz w usługach elektrycznych. Łączymy
+    precyzję rzemiosła z nowoczesnymi technologiami, aby dostarczać produkty
+    najwyższej jakości, dostosowane do potrzeb naszych klientów. Nasza pasja do
+    tworzenia trwałych i estetycznych rozwiązań oraz profesjonalne podejście do
+    każdego projektu gwarantują satysfakcję i niezawodność.
+projects:
+  title: Nasze realizacje
+  button: Zobacz więcej
+---
+
---
+++ tina/config.ts
@@ -0,0 +1,245 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "home",
+        label: "Strona Główna",
+        path: "content/home",
+        fields: [
+          {
+            type: "string",
+            name: "title1",
+            label: "Tytuł - (część pierwsza)",
+          },
+          {
+            type: "string",
+            name: "title2",
+            label: "Tytuł - (część druga)",
+          },
+          {
+            type: "string",
+            name: "description1",
+            label: "Opis - (część pierwsza)",
+          },
+          {
+            type: "string",
+            name: "description2",
+            label: "Opis - (część druga)",
+          },
+          {
+            type: "string",
+            name: "button",
+            label: "Przycisk - Napisz do nas",
+          },
+          {
+            type: "object",
+            name: "cards",
+            label: "Karty",
+            fields: [
+              {
+                type: "object",
+                name: "card1",
+                label: "Karta 1",
+                fields: [
+                  {
+                    type: "string",
+                    name: "title",
+                    label: "Tytuł Karty 1",
+                  },
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Opis Karty 1",
+                  },
+                ],
+              },
+              {
+                type: "object",
+                name: "card2",
+                label: "Karta 2",
+                fields: [
+                  {
+                    type: "string",
+                    name: "title",
+                    label: "Tytuł Karty 2",
+                  },
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Opis Karty 2",
+                  },
+                ],
+              },
+              {
+                type: "object",
+                name: "card3",
+                label: "Karta 3",
+                fields: [
+                  {
+                    type: "string",
+                    name: "title",
+                    label: "Tytuł Karty 3",
+                  },
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Opis Karty 3",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: "object",
+            name: "about",
+            label: "O nas",
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Tytuł sekcji O nas",
+              },
+              {
+                type: "string",
+                name: "description",
+                label: "Opis sekcji O nas",
+              },
+            ],
+          },
+          {
+            type: "object",
+            name: "projects",
+            label: "Projekty",
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Tytuł sekcji Projekty",
+              },
+              {
+                type: "string",
+                name: "button",
+                label: "Przycisk - Zobacz więcej",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "contact",
+        label: "Kontakt",
+        path: "content/contact",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Tytuł - Odezwij się do nas",
+          },
+          {
+            type: "string",
+            name: "title2",
+            label: "Tytuł - Masz pomysł na realizację?",
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Opis",
+          },
+          {
+            type: "object",
+            name: "form",
+            label: "Formularz",
+            fields: [
+              {
+                type: "string",
+                name: "name",
+                label: "Pole - Imię",
+              },
+              {
+                type: "string",
+                name: "email",
+                label: "Pole - Adres e-mail",
+              },
+              {
+                type: "string",
+                name: "phone",
+                label: "Pole - Numer telefonu",
+              },
+              {
+                type: "string",
+                name: "button",
+                label: "Przycisk - Wyślij",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "footer",
+        label: "Stopka",
+        path: "content/footer",
+        fields: [
+          {
+            type: "string",
+            name: "contact",
+            label: "Kontakt",
+          },
+          {
+            type: "string",
+            name: "rules",
+            label: "Polityka prywatności i warunki",
+          },
+        ],
+      },
+      {
+        name: "navbar",
+        label: "Nawigacja",
+        path: "content/navbar",
+        fields: [
+          {
+            type: "string",
+            name: "main",
+            label: "Strona główna",
+          },
+          {
+            type: "string",
+            name: "projects",
+            label: "Projekty",
+          },
+          {
+            type: "string",
+            name: "contact",
+            label: "Kontakt",
+          },
+        ],
+      },
+    ],
+  },
+});
