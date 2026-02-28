013e986d4f10c0a9e3b878fdf1df14930cd56ed6
mcho <mateusz.chodacki@gmail.com>
2026-02-28 16:35:45 +0100

    Seo fixes

--- astro-custom-steel/astro.config.mjs
+++ astro-custom-steel/astro.config.mjs
@@ -4,6 +4,8 @@ import { loadEnv } from "vite"; // 1. Importujemy loadEnv
 import sanity from '@sanity/astro';
 import icon from 'astro-icon';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // 2. Pobieramy zmienne środowiskowe ręcznie
 const { SANITY_PROJECT_ID, NEXT_PUBLIC_SANITY_DATASET } = loadEnv(
@@ -14,6 +16,7 @@ const { SANITY_PROJECT_ID, NEXT_PUBLIC_SANITY_DATASET } = loadEnv(
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://custom-steel.eu',
   output: 'server',
   adapter: netlify(),
   i18n: {
@@ -32,5 +35,7 @@ export default defineConfig({
       useCdn: false,
     }),
     icon(),
+    sitemap(),
+    robotsTxt(),
   ]
 });
